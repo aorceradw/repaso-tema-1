72f48523e3222243be1b1ecee3671d0e81a9ccf7
Angela <aorceradw@gmail.com>
2026-06-15 00:54:55 +0200

    ejercicio 28 enteri

---
+++ ejercicio28.py
@@ -0,0 +1,7 @@
+alumno = {}
+
+alumno["nombre"] = input("Di tu nombre: ")
+alumno ["edad"] = input ("Di tu edad: ")
+alumno ["curso"] = input ("Di tu curso: ")
+
+print(f"Eres {alumno["nombre"]} tienes, {alumno["edad"]} y tu curso es {alumno["curso"]}")
