1552a2cf14c92bbb2ab17326db43e4597e28b89a
Angela <aorceradw@gmail.com>
2026-06-14 17:07:53 +0200

    ejercicio 11 completo

---
+++ ejercicio11.py
@@ -0,0 +1,7 @@
+contraseña_correcta = "python123"
+contraseña =input("Inserte una contraseña: ")
+
+while contraseña_correcta != contraseña:
+    print("Contraseña incorrecta")
+    contraseña = input("Insserte contraseña:")
+print("CONTRASEÑA CORRECTA YIPPIE YAAAAAAAAAAAY")
